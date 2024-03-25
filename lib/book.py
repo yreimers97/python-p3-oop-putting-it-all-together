f29ccefed044b104b20b67ce1a54e3b512d818b1
Yuri <+>
2024-03-25 01:30:38 -0400

    Completed OOP part 1

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
+import ipdb
 
 class Book:
-    pass
-    
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
         
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count_parameter):
+        if(type(page_count_parameter) == int):
+            self._page_count = page_count_parameter
+        else:
+            print("page_count must be an integer")
+        
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+        
+    def __repr__(self):
+        return f"Book name is {self.title} and the page I am on is {self.page_count}"
+
+#ipdb.set_trace()
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size_parameter):
+        if(type(size_parameter) == int):
+            self._size = size_parameter
+        else:
+            print('size must be an integer')
+
+    def cobble(self):
+        print('Your shoe is as good as new!')
+        self.condition = 'New'
